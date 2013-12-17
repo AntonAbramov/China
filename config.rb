3c9e8cc0c0d49a8286fd7c31bd6f1364f6de8a3e
anton <AntonAbramov1990@gmail.com>
2013-12-17 23:02:08 +0200

    update files

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = ".."
+http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
@@ -14,7 +14,7 @@ javascripts_dir = "js"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = true
+# line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
@@ -22,21 +22,3 @@ line_comments = true
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-
-# Make a copy of sprites with a name that has no uniqueness of the hash.
-on_sprite_saved do |filename|
-  if File.exists?(filename)
-    FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
-  end
-end
-
-# Replace in stylesheets generated references to sprites
-# by their counterparts without the hash uniqueness.
-on_stylesheet_saved do |filename|
-  if File.exists?(filename)
-    css = File.read filename
-    File.open(filename, 'w+') do |f|
-      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
-    end
-  end
-end
