f8a39cad7dc1c915d54d028d115aff7adcc5cfff
anton <AntonAbramov1990@gmail.com>
2013-12-18 22:24:16 +0200

    header cart and call-back html and js logic

--- js/init.js
+++ js/init.js
@@ -1,7 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+	// SEARCH
+	$("#srch").keypress(function () {
+		$("#srch-popup").show();
+	});
+	// клик вне области окна что бы закрыть само окно.
+	$(document).click(function (event) {
+		if ($(event.target).closest("#srch-popup").length) return;
+		$("#srch-popup").hide();
+		event.stopPropagation();
+	});
+
+	// CALL-BACK
+	$("#btn-call-back").on("click", function(){
+		$("#call-back-popup").fadeIn();
+	});
+	$("#call-back-popup").find(".popup").on("click", function(event){
+		event.stopPropagation();
+	});
+	$("#call-back-popup .close, #call-back-popup, #callme").on("click", function(){
+		closeCallBack()
+	});
+});
+
+$(window).load(function () {
 
 });
 
-$(window).load(function(){
 
-});
+closeCallBack = function(){
+	$("#call-back-popup").fadeOut();
+}
