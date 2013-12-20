e50663d5a6ce4acbd78912bf787f7d795190d699
anton <AntonAbramov1990@gmail.com>
2013-12-20 09:39:45 +0200

    basket page

--- js/init.js
+++ js/init.js
@@ -21,6 +21,37 @@ $(document).ready(function () {
 	$("#call-back-popup .close, #call-back-popup, #callme").on("click", function(){
 		closeCallBack()
 	});
+
+
+	//BASKET
+
+	$("#cart-btn").on("click", function(){
+		$(".basket").fadeToggle();
+	});
+	$("#close-basket-window").on("click", function(){
+		$(".basket").fadeOut();
+	});
+
+	$('.number-grow').find('.plus').on('click', function(){
+		var currentInput = $(this).parents('.number-grow').find('input');
+		var inputVal = parseInt( currentInput.val());
+		var newInputVal = inputVal + 1;
+		currentInput.val(newInputVal);
+	});
+	$('.number-grow').find('.min').on('click', function(){
+		var currentInput = $(this).parents('.number-grow').find('input');
+		var inputVal = parseInt( currentInput.val());
+		var newInputVal = inputVal - 1;
+		if(newInputVal > 0) {
+		    currentInput.val(newInputVal);
+		}
+	});
+
+
+	//Delete item from Basket cart
+	$(document).on('click', ".basket-remove .remove", function(){
+		$(this).parents('tr').remove();
+	})
 });
 
 $(window).load(function () {
