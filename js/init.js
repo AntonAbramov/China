a48e893b0067d0a9323a1ce0adfdceea86606880
anton <AntonAbramov1990@gmail.com>
2013-12-22 20:54:41 +0200

    add sidebar and slider

--- js/init.js
+++ js/init.js
@@ -55,7 +55,21 @@ $(document).ready(function () {
 });
 
 $(window).load(function () {
-
+	$('#slides').slidesjs({
+		width: 940,
+		height: 528,
+		navigation: {
+			effect: "fade"
+		},
+		pagination: {
+			effect: "fade"
+		},
+		effect: {
+			fade: {
+				speed: 400
+			}
+		}
+	});
 });
 
 
